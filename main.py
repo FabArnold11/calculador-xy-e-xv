8eab7863ef03c209cfa3799aaf81cc77a8c3da30
FabArnold11 <fabiosantos1108@gmail.com>
2023-03-08 17:30:27 -0300

    calculadora_xv_e_yv

---
+++ main.py
@@ -0,0 +1,12 @@
+a = float(input("Digite o valor de a:"))
+
+b = float(input("Digite o valor de b:"))
+
+c = float(input("Digite o valor de c:"))
+
+xv = -b/(2*a)
+
+yv = -(b**2-4*a*c)/(4*a)
+
+print("xv=",xv)
+print("yv=",yv)
